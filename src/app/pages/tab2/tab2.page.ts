f1bdf2165f263d9638ad45d4d9b72aaa3234a33f
jganchozo <jganchozo@outlook.com>
2021-03-05 09:50:58 -0500

    select value and load information

--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { IonSegment } from '@ionic/angular';
+import { Article } from 'src/app/interfaces/interfaces';
 import { NoticiasService } from 'src/app/services/noticias.service';
 
 @Component({
@@ -12,14 +13,27 @@ export class Tab2Page implements OnInit {
   @ViewChild(IonSegment, { static: true }) segment: IonSegment;
 
   categorias = ['business','entertainment', 'general', 'health', 'science', 'sports', 'technology'];
+  noticias: Article[] = [];
 
   constructor(private noticiasService: NoticiasService) {}
 
   ngOnInit(){
     this.segment.value = this.categorias[0];
-    this.noticiasService.getTopHeadlinesCategoria(this.categorias[0]).subscribe(noticias => {
-      console.log(noticias);
-    });
+
+    this.cargarNoticias(this.categorias[0]);
   }
 
+  cambioCategoria = (event) => {
+    this.noticias = [];
+    this.cargarNoticias(event.detail.value);
+  }
+
+  cargarNoticias = (categoria: string) => {
+
+    this.noticiasService.getTopHeadlinesCategoria(categoria).subscribe(noticias => {
+      console.log(noticias);
+      this.noticias.push(...noticias.articles);
+    });
+
+  }
 }
